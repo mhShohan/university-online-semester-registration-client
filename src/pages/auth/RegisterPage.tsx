d6acb750d10ace310f600d95921a7d133cf27157
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-12-29 20:34:46 +0600

    stepper implemented to RegisterForm

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,7 +1,12 @@
 import { Container } from '@mui/material';
+import RegisterForm from '../../components/RegisterForm';
 
 const RegisterPage = () => {
-  return <Container sx={{ height: '100vh' }}>RegisterPage</Container>;
+  return (
+    <Container sx={{ height: '100vh' }}>
+      <RegisterForm />
+    </Container>
+  );
 };
 
 export default RegisterPage;
