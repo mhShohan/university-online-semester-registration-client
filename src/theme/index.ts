eedbae34f9614b4165b3f6be97b548b49bc88c2b
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-12-30 18:56:28 +0600

    theme color implemented

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,19 +5,19 @@ let theme = createTheme({
     primary: {
       dark: '#092635',
       main: '#1B4242',
-      light: '#9EC8B9',
+      light: '#9EC8B9'
     },
     secondary: {
       dark: '#18122B',
       main: '#443C68',
-      light: '#635985',
+      light: '#635985'
     },
     text: {
       primary: '#191919',
       secondary: '#384137',
       disabled: '#607274'
     }
-  },
+  }
 });
 
 export default theme;
