10673d0992d85e10612fd33c27f973faab97344f
Mehdi Hasan  Shohan <mehdihasanshohan25@gmail.com>
2024-05-19 18:22:01 +0600

    update github ations

--- src/store/features/baseApi.ts
+++ src/store/features/baseApi.ts
@@ -13,7 +13,7 @@ import config from '../../config';
 import tagTypesList from '../../constants/tagTypes';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: config.localUrl,
+  baseUrl: config.cloudUrl,
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
 
